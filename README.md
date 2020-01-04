3566b175b78e048106186c632da5c3e8854bc95f
ric-linux <solidkind@gmail.com>
2020-01-04 23:08:27 +0100

    - Update README.md - Implemented "add" command

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # minimalist-git-client
-Minimalist Git Client made with PyGtk
+I made this program only for make practice with pygtk library and Glade.
 
-This program at the moment has these functionality
+This program at the moment support only these functionality
 
 - Initialize repository
-- add command
+- "add" command
--- minimalit_git_client.py
+++ minimalit_git_client.py
@@ -1,8 +1,12 @@
 import gi
+import git
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Handler(Gtk.Window):
+
+    _path_selected = ""
+    _repo_obj = None
 
     def onDestroy(self, *args):
         Gtk.main_quit()
@@ -13,19 +17,25 @@ class Handler(Gtk.Window):
             Gtk.FileChooserAction.SELECT_FOLDER,
             (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
              "Select", Gtk.ResponseType.OK))
+
         dialog.set_default_size(800, 400)
 
         response = dialog.run()
+
         if response == Gtk.ResponseType.OK:
-            print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
+
+            Handler._path_selected = dialog.get_filename()
+            Handler._repo_obj = git.Repo.init(Handler._path_selected)
+
         elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
+
+            pass
 
         dialog.destroy()
 
     def add_file_repository_clicked(self, button):
-        print("Hello World!")
+
+        Handler._repo_obj.git.add(all = True)
 
 builder = Gtk.Builder()
 builder.add_from_file("minimalist_git_client_ui.glade")
